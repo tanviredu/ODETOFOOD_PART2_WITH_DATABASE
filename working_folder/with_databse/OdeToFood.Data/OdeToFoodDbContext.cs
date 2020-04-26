57538decb667838a22523f2ada07983aaae65116
tanviredu <ornobtanvir.git@gmail.com>
2020-04-26 10:08:39 +0600

    added

---
+++ working_folder/with_databse/OdeToFood.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+
+    // implement the db Context
+    public class OdeToFoodDbContext:DbContext
+    {
+        // add Dbset type with Resturant properties classs
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+    }
+}
